e5eda12d906cb5e5ee5c4f7bb28da53d6386231a
Juno Im <junorouse@gmail.com>
2022-02-08 15:18:25 +0900

    hands-on-001

---
+++ hands-on-001/main.js
@@ -0,0 +1,14 @@
+window.addEventListener("hashchange", handleHashChange, false);
+
+const display = document.getElementById('display');
+const style = document.getElementById('styleElement');
+
+function handleHashChange(e) {
+    const hash = location.hash.substr(1);
+    const up = new URLSearchParams(hash);
+    const theme = up.get('theme');
+
+    style.innerText = `body { background-color: ${theme}}`;
+}
+
+handleHashChange(); // first hit
